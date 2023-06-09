c04b7a0ce1d130dab44b9309b8433f68a2af899a
flavioborgesnunes <flavioborgesnunes@gmail.com>
2023-06-09 16:57:51 -0300

    Configuração de URL's e Autenticação Básica

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,44 @@
-from django.http import JsonResponse
+from rest_framework import viewsets, generics
+from escola.models import Aluno, Curso, Matricula
+from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer, ListaAlunosMatriculadosSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-def alunos(request):
-    if request.method == 'GET':
-        aluno ={'id' : 1, 'nome':'Flavio'}
-        return JsonResponse(aluno)
+class AlunosViewSet(viewsets.ModelViewSet):
+    """ Exibindo todos os alunos e alunas"""
+    queryset =  Aluno.objects.all() 
+    serializer_class = AlunoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+class CursosViewsSet(viewsets.ModelViewSet):
+    """ Exibindo todos os cursos"""
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+class MatriculaViewSet(viewsets.ModelViewSet):
+    """ Listando todas as matículas """
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+class ListaMatriculasAluno(generics.ListAPIView):
+    """ Listando as matrículas de um aluno """
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(aluno_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasAlunoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+class ListaAlunosMariculados(generics.ListAPIView):
+    """ listando alunos mariculados em um curso """
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaAlunosMatriculadosSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
--- setup/urls.py
+++ setup/urls.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from django.urls import path
-from escola.views import alunos
+from django.urls import path, include
+from escola.views import AlunosViewSet, CursosViewsSet, MatriculaViewSet, ListaMatriculasAluno, ListaAlunosMariculados
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('alunos', AlunosViewSet, basename='Alunos')
+router.register('cursos', CursosViewsSet, basename='Cursos')
+router.register('matriculas', MatriculaViewSet, basename='Matricula')
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("alunos/", alunos),
+    path("", include(router.urls)),
+    path("aluno/<int:pk>/matriculas/", ListaMatriculasAluno.as_view()),
+    path("curso/<int:pk>/matriculas/", ListaAlunosMariculados.as_view())
 ]
